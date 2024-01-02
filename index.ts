c1d73afed27b79e94d9a693cd1bc2a63e7b31977
TejaSaii <tejas7833@gmail.com>
2024-01-03 03:55:49 +0530

    fixed port value

--- index.ts
+++ index.ts
@@ -3,12 +3,12 @@ const app = express();
 import authRoutes from './routes/auth';
 import videoRoutes from './routes/video'
 import mongoose from "mongoose";
-const port = 3000;
 import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
 const dbhost = process.env.DB_HOST as string;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
